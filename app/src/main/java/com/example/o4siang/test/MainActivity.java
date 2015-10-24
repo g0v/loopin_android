1e4630ef3ca24e060dc13dd8dc096a48c653c2d8
Shawn Lin <O4siang@Shawns-Mac.local>
2015-10-24 16:46:54 +0800

    gps not move

--- app/src/main/java/com/example/o4siang/test/MainActivity.java
+++ app/src/main/java/com/example/o4siang/test/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.o4siang.test;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.app.Activity;
@@ -12,14 +14,28 @@ import android.view.Window;
 import android.view.WindowManager;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.content.Context;
+import android.content.Intent;
+import android.location.Criteria;
+import android.provider.Settings;
 
-public class MainActivity extends Activity implements SensorEventListener {
+
+import static java.lang.Math.sqrt;
+
+public class MainActivity extends Activity implements SensorEventListener, LocationListener {
     private TextView text_x;
     private TextView text_y;
     private TextView text_z;
+    private TextView text_all;
     private SensorManager aSensorManager;
     private Sensor aSensor;
     private float gravity[] = new float[3];
+    private boolean getService = false;
+    private LocationManager lms;
+    private String bestProvider = LocationManager.GPS_PROVIDER;    //最佳資訊提供者
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -29,28 +45,84 @@ public class MainActivity extends Activity implements SensorEventListener {
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
 
-        text_x = (TextView)findViewById(R.id.TextView01);
-        text_y = (TextView)findViewById(R.id.TextView02);
-        text_z = (TextView)findViewById(R.id.TextView03);
-
-        aSensorManager=(SensorManager) getSystemService(SENSOR_SERVICE);
+        text_x = (TextView) findViewById(R.id.TextView01);
+        text_y = (TextView) findViewById(R.id.TextView02);
+        text_z = (TextView) findViewById(R.id.TextView03);
+        text_all = (TextView) findViewById(R.id.TextView04);
+        aSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
         aSensor = aSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
         aSensorManager.registerListener(this, aSensor, aSensorManager.SENSOR_DELAY_NORMAL);
+
+        LocationManager status = (LocationManager) (this.getSystemService(Context.LOCATION_SERVICE));
+        if (status.isProviderEnabled(LocationManager.GPS_PROVIDER) || status.isProviderEnabled(LocationManager.NETWORK_PROVIDER)) {
+            //如果GPS或網路定位開啟，呼叫locationServiceInitial()更新位置
+            getService = true;
+            locationServiceInitial();
+        }
+        else {
+            Toast.makeText(this, "請開啟定位服務", Toast.LENGTH_LONG).show();
+            startActivity(new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS));    //開啟設定頁面
+        }
     }
+
+
+
+    private void locationServiceInitial() {
+        lms = (LocationManager) getSystemService(LOCATION_SERVICE);    //取得系統定位服務
+
+
+        Criteria criteria = new Criteria();    //資訊提供者選取標準
+        bestProvider = lms.getBestProvider(criteria, true);
+        Location location = lms.getLastKnownLocation(bestProvider);    //使用GPS定位座標
+        getLocation(location);
+    }
+
+    private void getLocation(Location location) {    //將定位資訊顯示在畫面中
+        if (location != null) {
+            TextView longitude_txt = (TextView) findViewById(R.id.longitude);
+            TextView latitude_txt = (TextView) findViewById(R.id.latitude);
+
+            Double longitude = location.getLongitude();    //取得經度
+            Double latitude = location.getLatitude();    //取得緯度
+
+            longitude_txt.setText(String.valueOf(longitude));
+            latitude_txt.setText(String.valueOf(latitude));
+        } else {
+            Toast.makeText(this, "無法定位座標", Toast.LENGTH_LONG).show();
+        }
+    }
+
+
     @Override
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
 // TODO Auto-generated method stub
 
     }
+
     @Override
     public void onSensorChanged(SensorEvent event) {
 // TODO Auto-generated method stub
         gravity[0] = event.values[0];
         gravity[1] = event.values[1];
         gravity[2] = event.values[2];
-        text_x.setText("X = "+gravity[0]);
-        text_y.setText("Y = "+gravity[1]);
-        text_z.setText("Z = "+gravity[2]);
+        float total = gravity[0] * gravity[0] + gravity[1] * gravity[1] + gravity[2] * gravity[2];
+        float result;
+        result = (float) sqrt(total);
+        text_x.setText("X = " + gravity[0]);
+        text_y.setText("Y = " + gravity[1]);
+        text_z.setText("Z = " + gravity[2]);
+        text_all.setText("震動 ＝ " + result);
+    }
+
+    @Override
+    protected void onResume() {
+        // TODO Auto-generated method stub
+        super.onResume();
+        if (getService) {
+
+            lms.requestLocationUpdates(bestProvider, 100, 1, this);
+            //服務提供者、更新頻率60000毫秒=1分鐘、最短距離、地點改變時呼叫物件
+        }
     }
     @Override
     protected void onPause()
@@ -60,5 +132,35 @@ public class MainActivity extends Activity implements SensorEventListener {
         aSensorManager.unregisterListener(this);
         Toast.makeText(this, "Unregister accelerometerListener", Toast.LENGTH_LONG).show();
         super.onPause();
+        if(getService) {
+            lms.removeUpdates(this);	//離開頁面時停止更新
+        }
+
     }
+
+    @Override
+    public void onLocationChanged(Location location) {	//當地點改變時
+        // TODO Auto-generated method stub
+        getLocation(location);
+    }
+
+    @Override
+    public void onProviderDisabled(String arg0) {	//當GPS或網路定位功能關閉時
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void onProviderEnabled(String arg0) {	//當GPS或網路定位功能開啟
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {	//定位狀態改變
+        //status=OUT_OF_SERVICE 供應商停止服務
+        //status=TEMPORARILY_UNAVAILABLE 供應商暫停服務
+    }
+
+
 }
